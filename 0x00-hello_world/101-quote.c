d8879d3d96802729c1482040d430edd447a4e57f
PhantomCoder1 <DesmondOgolo1@gmail.com>
2023-03-20 13:21:29 -0700

    low level programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-*main-Output in standard error
+*main- standard error
 *Return: always 1 (success)
 */
 int main(void)
 {
-	fputs("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
